c611504cc03ce03c99829ca91def1df5b198ed57
Zelda <166204162+Zeldazsy@users.noreply.github.com>
2025-04-26 21:13:44 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true, // must be true
-    typedRoutes: true, // optional but useful
+    appDir: true,
+    typedRoutes: true,
+    optimizeCss: true,
+    optimizeFonts: true,
+    serverActions: true,
+    serverRendering: 'blocking', // <- เพิ่มบรรทัดนี้
   },
+  compress: true,
 }
 
 export default nextConfig
